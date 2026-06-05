5179aeffde5eb4569e87f5829b54e3d123aae5f9
Yahya Farag <115194344+ywf1@users.noreply.github.com>
2026-06-05 15:05:29 -0400

    Update payloadSTMmain.ino
    gyro integration fix

--- embeddedSTM/payloadSTMmain/payloadSTMmain.ino
+++ embeddedSTM/payloadSTMmain/payloadSTMmain.ino
@@ -321,13 +321,15 @@ void loop() {
     gyroReady = false;
 
     currentTime = micros();
-    unsigned long gyroDt = (currentTime-lastGyroTime) / 1.0e6;
+    float gyroDt = (currentTime-lastGyroTime) / 1.0e6;
     lastGyroTime = currentTime;
 
     sensors_event_t gyro;
     lsm6dsox.getEvent(NULL, &gyro, NULL);
     //integrate dps of each axis to get angle
-    totalZrot += gyro.gyro.x; totalYrot = gyro.gyro.y; totalZrot = gyro.gyro.z;
+    totalXrot += gyro.gyro.x * gyroDt;
+    totalYrot = gyro.gyro.y * gyroDt;
+    totalZrot = gyro.gyro.z * gyroDt;
   }
 
   //light sensor updating:
